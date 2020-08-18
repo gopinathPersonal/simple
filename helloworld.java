2c2f6d5187139441c580619949a89663a8a22e62
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-08-17 21:36:26 -0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Executed successfully....!!!! - Hari");
+        System.out.println("Welcome to Jenkins.. Gopi!!!");
  //creating object
   }
 }
