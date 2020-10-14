90eef1ee13c3eb2c40e7089dc531f1f28fc09fd2
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-10-14 12:09:50 -0700

    Update ins-new.py

--- ins-new.py
+++ ins-new.py
@@ -2,8 +2,8 @@ import boto3
 import sys
 
 region = 'us-west-2'
-accesskey = sys.argv[1]
-secretkey = sys.argv[2]
+accesskey = sys.argv[0]
+secretkey = sys.argv[1]
 
 
 client = boto3.client('ec2',region_name=region, aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
