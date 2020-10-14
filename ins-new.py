7ecee1fe220d6dacda5cea4968172366da054638
gopinathPersonal <63572096+gopinathPersonal@users.noreply.github.com>
2020-10-14 12:51:27 -0700

    Update ins-new.py

--- ins-new.py
+++ ins-new.py
@@ -1,8 +1,8 @@
 import boto3
 import sys
 
-accesskey = 'AKIAQS5DASRCGEER732K'
-secretkey = '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'
+accesskey = sys.argv[2]
+secretkey = sys.argv[3]
 
 
 client = boto3.client('ec2','us-west-2', aws_access_key_id=accesskey, aws_secret_access_key=secretkey)
